b05d3e253bc0b7ce581efb0fe69287ca7eff06a3
jefby <jef199006@gmail.com>
2018-08-30 19:31:57 +0000

    fix docker build failed issue
    issue:
    
    ```
    /usr/local/intel/mkldnn/include/mkldnn.hpp:192:62: error: exception handling disabled, use -fexceptions to enable
                     throw error(status, message, *error_primitive);
                                                                  ^
    In file included from ten
    ```

--- build/build.sh
+++ build/build.sh
@@ -950,7 +950,7 @@ index 1a1ab54..11c598f 100644
 +BLAS?=MKL
 +BLAS_CXX_FLAGS/ATLAS:=-DEIGEN_USE_BLAS -DEIGEN_USE_LAPACKE
 +BLAS_CXX_FLAGS/OpenBLAS:=-DEIGEN_USE_BLAS -DEIGEN_USE_LAPACKE
-+BLAS_CXX_FLAGS/MKL:=${mkl_cxxflags}
++BLAS_CXX_FLAGS/MKL:=${mkl_cxxflags} -fexceptions
 +BLAS_LD_FLAGS/ATLAS:=-L${prefix}/ATLAS/lib -llapack -lcblas -lf77blas -latlas -lgfortran -lquadmath
 +BLAS_LD_FLAGS/OpenBLAS:=-L${prefix}/OpenBLAS/lib -lopenblas -lgfortran -lquadmath
 +# See https://software.intel.com/en-us/articles/intel-mkl-link-line-advisor/
